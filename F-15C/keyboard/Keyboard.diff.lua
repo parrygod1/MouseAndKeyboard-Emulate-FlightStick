38e348a0d408efaf4489be6b694735da8fd2d5e9
Paraschiv Tudor <gparaschiv@gmail.com>
2022-03-26 06:40:10 +0200

    f 15

---
+++ F-15C/keyboard/Keyboard.diff.lua
@@ -0,0 +1,33 @@
+local diff = {
+	["keyDiffs"] = {
+		["d102pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "J",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+				[2] = {
+					["key"] = "Home",
+				},
+			},
+			["name"] = "Next Waypoint, Airfield Or Target",
+		},
+		["d1315pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "J",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+				[2] = {
+					["key"] = "End",
+				},
+			},
+			["name"] = "Previous Waypoint, Airfield Or Target",
+		},
+	},
+}
+return diff
---
+++ F-15C/trackir/TrackIR.diff.lua
@@ -0,0 +1,93 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2010cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "TRACKIR_YAW",
+				},
+			},
+			["name"] = "Absolute Camera Horizontal View",
+		},
+		["a2011cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "TRACKIR_PITCH",
+				},
+			},
+			["name"] = "Absolute Camera Vertical View",
+		},
+		["a2049cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "TRACKIR_X",
+				},
+			},
+			["name"] = "Absolute Horizontal Shift Camera View",
+		},
+		["a2051cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "TRACKIR_Y",
+				},
+			},
+			["name"] = "Absolute Vertical Shift Camera View",
+		},
+		["a2053cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "TRACKIR_Z",
+				},
+			},
+			["name"] = "Absolute Longitude Shift Camera View",
+		},
+		["a2144cdnil"] = {
+			["name"] = "Head Tracker : Yaw",
+			["removed"] = {
+				[1] = {
+					["key"] = "TRACKIR_YAW",
+				},
+			},
+		},
+		["a2145cdnil"] = {
+			["name"] = "Head Tracker : Pitch",
+			["removed"] = {
+				[1] = {
+					["key"] = "TRACKIR_PITCH",
+				},
+			},
+		},
+		["a2146cdnil"] = {
+			["name"] = "Head Tracker : Roll",
+			["removed"] = {
+				[1] = {
+					["key"] = "TRACKIR_ROLL",
+				},
+			},
+		},
+		["a2601cdnil"] = {
+			["name"] = "Head Tracker : Forward/Backward",
+			["removed"] = {
+				[1] = {
+					["key"] = "TRACKIR_Z",
+				},
+			},
+		},
+		["a2602cdnil"] = {
+			["name"] = "Head Tracker : Up/Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "TRACKIR_Y",
+				},
+			},
+		},
+		["a2603cdnil"] = {
+			["name"] = "Head Tracker : Right/Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "TRACKIR_X",
+				},
+			},
+		},
+	},
+}
+return diff
